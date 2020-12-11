89ab3456a7c7bfb4d9c6fd0b4c39e612876b538e
Pedram Valiani <p.valiani@gmail.com>
2020-12-11 19:31:39 +0000

    MVP complete

--- instrument_app/js/app.js
+++ instrument_app/js/app.js
@@ -1,21 +1,63 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
+  
+    const form = document.querySelector('#new-item-form');
+    form.addEventListener('submit', handleFormSubmit);
+  
+    const deleteButtonItem = document.createElement('button');
+    deleteButtonItem.id = 'delete';
+    deleteButtonItem.textContent = "delete all"
+    document.body.appendChild(deleteButtonItem);
+  
+    const button = document.querySelector('#delete');
+    button.addEventListener('click', handleButtonClick);
+  
+  })
+  
+  
+  const handleFormSubmit = function (event) {
+  
+    // stops the post request
+    event.preventDefault();
+  
+    const owner = event.target.owner.value
+    const model = event.target.model.value
+    const type = event.target.type.value
+  
+    // create li element i.e list
+    // 
+  
+    const newListItem = document.createElement('li');
+  
+    // Title
+    const newOwnerItem = document.createElement('h2');
+    newOwnerItem.textContent = `Owner: ${owner}`;
+    newListItem.appendChild(newOwnerItem);
+  
+    // Author
+    const newModelItem = document.createElement('h3');
+    newModelItem.textContent = `Model: ${model}`;
+    newListItem.appendChild(newModelItem);
+  
+    // Category
+  
+    const newInstrumentType = document.createElement('h4');
+    newInstrumentType.textContent = `Instrument Type: ${type}`;
+    newListItem.appendChild(newInstrumentType);
+  
+  
+    const instrumentList = document.querySelector("#instrument-list");
+  
+  
+    instrumentList.appendChild(newListItem);
+    event.target.reset();
+  };
+  
+  
+  const handleButtonClick = function (event) {
+    const instrumentList = document.querySelector("#instrument-list");
+    instrumentList.textContent = "";
+  
 
-const form = document.querySelector('#new-item-form'); // listen for the new-item-form id
-form.addEventListener('submit', handleFormSubmit); // when the submit i.e save button is clicked invoke the handleFormSubmit function
-
-// create a delete button in here which has an event listener for when clicked
-
-});
-
-
-const handleFormSubmit = function(event) {
-
-// stops the post request so that we can see our changes
-event.preventDefault();
-
-
-
-// resets the form
-event.target.reset();
-
-};
+  
+    
+  };
