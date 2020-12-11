2ec3468f7e6d8f493fe1fe16a0be18ee39523884
Pedram Valiani <p.valiani@gmail.com>
2020-12-11 18:29:18 +0000

    begin js and add dom_content_loaded

---
+++ instrument_app/js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+const form = document.querySelector('#new-item-form'); // listen for the new-item-form id
+form.addEventListener('submit', handleFormSubmit); // when the submit i.e save button is clicked invoke the handleFormSubmit function
+
+
+
+
+});
