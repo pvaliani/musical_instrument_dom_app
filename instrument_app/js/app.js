64c5e37933f46cdfd12b77271b265dc050efe8c2
Pedram Valiani <p.valiani@gmail.com>
2020-12-11 18:32:04 +0000

    adds stop post request and reset of form

--- instrument_app/js/app.js
+++ instrument_app/js/app.js
@@ -3,7 +3,19 @@ document.addEventListener('DOMContentLoaded', ()=>{
 const form = document.querySelector('#new-item-form'); // listen for the new-item-form id
 form.addEventListener('submit', handleFormSubmit); // when the submit i.e save button is clicked invoke the handleFormSubmit function
 
-
-
+// create a delete button in here which has an event listener for when clicked
 
 });
+
+
+const handleFormSubmit = function(event) {
+
+// stops the post request so that we can see our changes
+event.preventDefault();
+
+
+
+// resets the form
+event.target.reset();
+
+};
